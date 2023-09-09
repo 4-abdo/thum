9c6a6c7a28dd024ac2962f007f15de7077d183cb
AbdElaliOuami <ouamiabdelali01@gmail.com>
2023-09-09 03:32:24 +0200

    Modify Description

--- pages/index.js
+++ pages/index.js
@@ -34,10 +34,10 @@ const Index = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mxgggf-auto px-4 py-8">
       <header className="text-center mb-8">
         <h1 className="text-3xl font-bold mb-2">
-          Youtube Thumbnail Downloader
+          Free Youtube Thumbnail Downloader
         </h1>
         <p className="text-gray-600">
           Download high-quality thumbnails from YouTube videos.
